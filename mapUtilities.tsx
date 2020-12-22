03be51ce778b341e3a619a7085f1cbbfdfc61344
Stefan Schindler <dns2utf8@estada.ch>
2020-12-22 16:57:58 +0100

    Reset to https://git.cccv.de/rc3/world-map-starterkit.git

--- mapUtilities.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="structures/mapUtilities.png" width="288" height="32"/>
-</tileset>
